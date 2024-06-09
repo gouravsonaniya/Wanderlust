10aa985d35b9b99d3971418725496283c5d3517a
Gourav sonaniya <114682491+gouravsonaniya@users.noreply.github.com>
2024-06-09 20:37:05 +0530

    Add files via upload

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
  const initDB = async () => {
    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({...obj, owner: "65d516436fed1f137c2de55c"}));
    await Listing.insertMany(initData.data);
    console.log("data was initialized");
  };
